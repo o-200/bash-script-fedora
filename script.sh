23ad6db8e6351094aff03e65b1103b8b2ee29021
trava po poyas <34093966+o-200@users.noreply.github.com>
2024-08-22 16:14:23 +0700

    add IDE's

--- script.sh
+++ script.sh
@@ -61,6 +61,14 @@ dnf install discord
 # install using vpn, proxy
 flatpak install -y flathub com.jetbrains.RubyMine
 
+# VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf install code
+
+# Zed
+curl -f https://zed.dev/install.sh | sh
+
 #docker
 dnf install -y dnf-plugins-core
 dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
